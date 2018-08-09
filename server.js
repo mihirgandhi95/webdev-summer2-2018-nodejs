9081272fc106b2b06e8303e4d19cecc9e037a407
Mihir Gandhi <gandhi.mi@husky.neu.edu>
2018-08-08 22:25:25 -0400

    cors update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
+        req.headers.origin);
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
