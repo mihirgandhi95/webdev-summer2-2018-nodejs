f48f27e6159fd0ac81caff70fe7474667ff215d4
Mihir Gandhi <gandhi.mi@husky.neu.edu>
2018-08-16 20:05:20 -0400

    server for heroku

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(function(req, res, next) {
 
 
 
+    
+
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
